b3f079009a2227d6127f1dc706aa82906b42f1bf
shawnkoon <shawn9405@gmail.com>
2017-04-14 00:32:11 -0700

    Added bootstrap styling along with reason state

--- count-down-app/src/components/Clock.jsx
+++ count-down-app/src/components/Clock.jsx
@@ -2,12 +2,13 @@ import * as React from 'react';
 
 // Styles
 import '../styles/App.css';
+import { PageHeader } from 'react-bootstrap';
 
 export const Clock = props => (
-	<div>
+	<PageHeader>
 		<div className="Clock-days">{props.days} days</div>
 		<div className="Clock-hours">{props.hours} hours</div>
 		<div className="Clock-minutes">{props.minutes} minutes</div>
 		<div className="Clock-seconds">{props.seconds} seconds</div>
-	</div>
+	</PageHeader>
 );
--- count-down-app/src/containers/App.jsx
+++ count-down-app/src/containers/App.jsx
@@ -3,29 +3,65 @@ import * as React from 'react';
 // Container
 import { ClockContainer } from './Clock';
 
+// Styles
+import {
+	Button,
+	ControlLabel,
+	Form,
+	FormControl,
+	FormGroup,
+	Jumbotron,
+} from 'react-bootstrap';
+
 export class AppContainer extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			deadline: 'December 25, 2017',
 			newDeadline: '',
+			reasonText: 'Christmas!',
+			newReasonText: '',
 		}
 		this.changeDeadLine = this.changeDeadLine.bind(this);
 		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleReasonInputChange = this.handleReasonInputChange.bind(this);
 	}
 
 	render() {
 		return (
 			<div className="App">
-				<div className="App-title">Countdown to {this.state.deadline}</div>
+				<Jumbotron className="App-title">
+					Countdown to {this.state.deadline}
+					<p><strong>Reason : </strong>{this.state.reasonText}</p>
+				</Jumbotron>
 				<ClockContainer
 					deadline={this.state.deadline}
 				/>
-				<br/>
-				<div>
-					<input placeholder="new date" onChange={this.handleInputChange}/>
-					<button onClick={this.changeDeadLine}>Submit</button>
-				</div>
+				<Form inline>
+					<FormGroup>
+						<ControlLabel className="Input-label">Date :</ControlLabel>
+						<FormControl
+							className="Deadline-input"
+							placeholder="Ex) December 25, 2017"
+							onChange={this.handleInputChange}
+						/>
+					</FormGroup>
+					<br/>
+					<FormGroup>
+						<ControlLabel className="Input-label">Reason :</ControlLabel>
+						<FormControl
+							className="Reason-input"
+							placeholder="Ex) Christmas!"
+							onChange={this.handleReasonInputChange}
+						/>
+					</FormGroup>
+					<br/>
+					<Button
+						className="Submit-button"
+						bsStyle="success"
+						onClick={this.changeDeadLine}
+					>Submit</Button>
+				</Form>
 			</div>
 		);
 	}
@@ -37,9 +73,17 @@ export class AppContainer extends React.Component {
 		this.setState(input);
 	}
 
+	handleReasonInputChange(event) {
+		const input = {
+			newReasonText : event.target.value,
+		};
+		this.setState(input);
+	}
+
 	changeDeadLine() {
 		const newValue = {
 			deadline: this.state.newDeadline,
+			reasonText: this.state.newReasonText,
 		};
 		this.setState(newValue);
 	}
