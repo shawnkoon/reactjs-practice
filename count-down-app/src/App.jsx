22a38c77336d36ccb032704563f92a094ffe76ae
shawnkoon <shawn9405@gmail.com>
2017-04-13 22:56:24 -0700

    Added css along with states that updates based on input/button click

--- count-down-app/src/App.jsx
+++ count-down-app/src/App.jsx
@@ -2,21 +2,47 @@ import * as React from 'react';
 import './styles/App.css';
 
 export class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			deadline: 'December 25, 2017',
+			newDeadline: '',
+		}
+		this.changeDeadLine = this.changeDeadLine.bind(this);
+		this.handleInputChange = this.handleInputChange.bind(this);
+	}
+
 	render() {
 		return (
 			<div className="App">
-				<div>Countdown to December 25, 2017</div>
+				<div className="App-title">Countdown to {this.state.deadline}</div>
 				<div>
-					<div>14 days</div>
-					<div>30 hours</div>
-					<div>15 minutes</div>
-					<div>20 seconds</div>
+					<div className="Clock-days">14 days</div>
+					<div className="Clock-hours">30 hours</div>
+					<div className="Clock-minutes">15 minutes</div>
+					<div className="Clock-seconds">20 seconds</div>
 				</div>
+				<br/>
+				<br/>
 				<div>
-					<input placeholder="new date"/>
-					<button>Submit</button>
+					<input placeholder="new date" onChange={this.handleInputChange}/>
+					<button onClick={this.changeDeadLine}>Submit</button>
 				</div>
 			</div>
 		);
+	}
+
+	handleInputChange(event) {
+		const input = {
+			newDeadline : event.target.value,
+		};
+		this.setState(input);
+	}
+
+	changeDeadLine() {
+		const newValue = {
+			deadline: this.state.newDeadline,
+		};
+		this.setState(newValue);
 	}
 };
